de5e305b4f35360d500361e60c31effb4b11b52b
KenJ13 <kennethpardo3@gmail.com>
2024-09-03 18:37:01 -0500

    Archivo analisis_texto.php agregado

---
+++ PARCIALES/PARCIAL_1/analisis_texto.php
@@ -0,0 +1,7 @@
+<?php
+include 'analisis_texto.php';
+
+$arreglo_frases = ["Hola como estas", "Excelente!", "No puede ser!"];
+
+
+?>
--- PARCIALES/PARCIAL_1/utilidades_texto.php
+++ PARCIALES/PARCIAL_1/utilidades_texto.php
@@ -3,6 +3,15 @@
 function contar_palabras($texto){
 
 }
+
+
+function contar_vocales($texto){
+
+}
+
+function invertir_palabras($texto){
+    
+}
 /*cd C:/laragon/www
 git init
 git add .

b4a8b644a904073529c267411f6c720ae0b1ca1a
KenJ13 <kennethpardo3@gmail.com>
2024-09-03 20:08:58 -0500

    Creacion de tabla html con foreach

--- PARCIALES/PARCIAL_1/analisis_texto.php
+++ PARCIALES/PARCIAL_1/analisis_texto.php
@@ -1,7 +1,35 @@
 <?php
-include 'analisis_texto.php';
+include 'utilidades_texto.php';
 
-$arreglo_frases = ["Hola como estas", "Excelente!", "No puede ser!"];
-
-
+$frases = ["Hola como estás", "Excelente! dame tu número", "¡No puede ser! espero este bien."];
 ?>
+
+<!DOCTYPE html>
+<head>
+
+    <title>Análisis de Texto</title>
+</head>
+<body>
+    <h2>Análisis de Frases</h2>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Frase</th>
+                <th>Número de Palabras</th>
+                <th>Número de Vocales</th>
+                <th>Palabras Invertidas</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($frases as $frase): ?>
+                <tr>
+                    <td><?= htmlspecialchars($frase) ?></td>
+                    <td><?= contar_palabras($frase) ?></td>
+                    <td><?= contar_vocales($frase) ?></td>
+                    <td><?= htmlspecialchars(invertir_palabras($frase)) ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+</html>

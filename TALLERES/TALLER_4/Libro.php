56b1c4f649f77bdd89eb7f99db45f9754431fedc
KenJ13 <kennethpardo3@gmail.com>
2024-09-11 18:46:26 -0500

    Paso 2: Implementación de encapsulación y métodos getter/setter en la clase Libro

--- TALLERES/TALLER_4/Libro.php
+++ TALLERES/TALLER_4/Libro.php
@@ -10,13 +10,38 @@ class Libro {
         $this->anioPublicacion = $anioPublicacion;
     }
 
+    public function getTitulo() {
+        return $this->titulo;
+    }
+
+    public function setTitulo($titulo) {
+        $this->titulo = trim($titulo);
+    }
+
+    public function getAutor() {
+        return $this->autor;
+    }
+
+    public function setAutor($autor) {
+        $this->autor = trim($autor);
+    }
+
+    public function getAnioPublicacion() {
+        return $this->anioPublicacion;
+    }
+
+    public function setAnioPublicacion($anio) {
+        $this->anioPublicacion = intval($anio);
+    }
+
     public function obtenerInformacion() {
         return "'{$this->titulo}' por {$this->autor}, publicado en {$this->anioPublicacion}";
     }
+
 }
 
 // Ejemplo de uso
-$miLibro = new Libro("Cien años de soledad", "Gabriel García Márquez", 1967);
-echo $miLibro->obtenerInformacion();
+$miLibro = new Libro("  El Quijote  ", "Miguel de Cervantes",1965);
+echo $miLibro -> obtenerInformacion();
+echo "<br>Título: " . $miLibro -> getTitulo();
 
-        

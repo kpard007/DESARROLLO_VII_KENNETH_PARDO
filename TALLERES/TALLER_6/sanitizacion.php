617f13d7e498eea60e1e34660c6c169c7fb741da
KenJ13 <kennethpardo3@gmail.com>
2024-10-12 10:04:57 -0500

    TALLER_6 - Paso 3: Implementacion de funciones de sanitizacion de datos

---
+++ TALLERES/TALLER_6/sanitizacion.php
@@ -0,0 +1,32 @@
+<?php //cambiamos el fliter_sanitize_string por htmlspecialchars ya que esta depreciado
+function sanitizarNombre($nombre) {
+    return htmlspecialchars(trim($nombre), ENT_QUOTES, 'UTF-8');
+}
+
+function sanitizarEmail($email) {
+    return filter_var(trim($email), FILTER_SANITIZE_EMAIL);
+}
+
+function sanitizarEdad($edad) {
+    return filter_var($edad, FILTER_SANITIZE_NUMBER_INT);
+}
+
+function sanitizarSitioWeb($sitioWeb) {
+    return filter_var(trim($sitioWeb), FILTER_SANITIZE_URL);
+}
+
+function sanitizarGenero($genero) {
+    return htmlspecialchars(trim($genero), ENT_QUOTES, 'UTF-8');
+}
+
+function sanitizarIntereses($intereses) {
+    return array_map(function($interes) {
+        return htmlspecialchars(trim($interes), ENT_QUOTES, 'UTF-8');
+    }, $intereses);
+}
+
+function sanitizarComentarios($comentarios) {
+    return htmlspecialchars(trim($comentarios), ENT_QUOTES, 'UTF-8');
+}
+?>
+      
